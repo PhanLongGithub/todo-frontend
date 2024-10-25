b658b5f66921aea3cdf005e19d069b2c8ff1c4f1
Phan Kim Long <phanlong02.18@gmail.com>
2024-10-25 11:16:21 +0900

    Adding Authentication and Authorization

--- todo-frontend/src/App.jsx
+++ todo-frontend/src/App.jsx
@@ -5,19 +5,32 @@ import HeaderConponent from './components/HeaderConponent'
 import FooterComponent from './components/FooterComponent'
 import ListTodoComponent from './components/ListTodoComponent'
 import TodoComponent from './components/TodoComponent'
+import { isUserLoggedIn } from './service/AuthService'
+import LoginComponent from './components/LoginComponent '
+import RegisterComponent from './components/RegisterComponent'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  function AuthenticatedRoute({children}){
+    const isAuth = isUserLoggedIn();
+    if(isAuth) {
+      return children;
+    }
+    return <Navigate to="/" />
+  }
 
   return (
     <>
     <BrowserRouter>
         <HeaderConponent />
           <Routes>
-              <Route path='/' element = { <ListTodoComponent /> }></Route>
-              <Route path='/todos' element = { <ListTodoComponent /> }></Route>
-              <Route path='/add-todo' element = { <TodoComponent /> }></Route>
-              <Route path='/update-todo/:id' element = { <TodoComponent /> }></Route>
+              <Route path='/' element = { <LoginComponent /> }></Route>
+              <Route path='/todos' element = { <AuthenticatedRoute><ListTodoComponent /></AuthenticatedRoute> }></Route>
+              <Route path='/add-todo' element = { <AuthenticatedRoute><TodoComponent /></AuthenticatedRoute> }></Route>
+              <Route path='/update-todo/:id' element = { <AuthenticatedRoute><TodoComponent /></AuthenticatedRoute> }></Route>
+              <Route path='/register' element = { <RegisterComponent />}></Route>
+              <Route path='/login' element = { <LoginComponent /> }></Route>
           </Routes>
           
         <FooterComponent />
